add1984160ca4e4b99448f85e29b634735012454
Remi Gau <remi_gau@hotmail.com>
2022-08-31 14:18:14 +0200

    move info to README

--- Phono_localizer_tone_bisyll.m
+++ Phono_localizer_tone_bisyll.m
@@ -1,45 +1,3 @@
-%%%%% Phonology processing regions : LOCALIZER %%%%%%%
-%% script from Stefania Mattioni - adapted by Alice Van Audenhaege
-% Jan2022
-
-%% RUN DESCRIPTION
-% There are 20 blocks.
-% Categories of stimuli = 2 (syllable and scramble). Alternation
-% SYLL-SCR-SYLL-SCR-....
-% 10 fixed sequences of 15 bisyll pseudowords. 1 sequence presented per block.
-% Order of sequences randomized for each particpant.
-
-%% BLOCK DESCRIPTION
-% In each block, all the stimuli of the sequence are presented sequentially.
-% Trial duration = duration of each audio file + n msec to arrive to
-% 1.2s/trial.  ?????????? OR 1s ????????????
-% In each block there are either 0, 1 or 2 (randomly decided) targets.
-% The participant has to press when he/she hears a target.
-% A target is a pure tone (duration 0.5s).
-% Each block has a duration of 18, 19, 20 s (depending if 0, 1 or 2 targets).
-
-% TIME CALCULATION for a RUN
-% 2 categories with 15 stimulus each one;
-% trial duration= 1.2s (stim of various duration + ISI);
-% block duration = 1 category in each block + 0/1/2 targets = 18/19/20s
-
-% 1 pause of 8s at the beginning of the run
-% 20 blocks per run : minimum 360s / maximum 400s (according to 0, 1 or 2
-% targets)
-% 20 pauses of 6s = 120s
-%
-% MINIMUM DURATION = 488s (8min08sec) / MAXIMUM DURATION = 528s (8min48sec)
-% Fixation cross to fill the time difference to get to 528s anyway.
-
-% ACTION and VARIABLE SETTING
-% The only variable you need to manually change is Cfg.device at the
-% beginning of the script. Put either 'PC' or 'Scanner'.
-% Once you will Run the script you will be asked to select some variables:
-% 1. Group (TO DEFINE): %%for the moment only controls CON is defined as
-% default
-% 2. SubID : first 2 letters of Name + first 2 letters of Surname (e.g. Stefania Mattioni == StMa).
-% 3. Run Number : 1st or 2nd run
-
 clear all;
 clc;
 Screen('Preference', 'SkipSyncTests', 1);
@@ -143,7 +101,7 @@ try  % safety loop: close the screen if code crashes
     %% TRIGGER
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     Screen('TextSize', wPtr, 50); % text size
-    DrawFormattedText(wPtr, '\n READY TO START \n \n - Détectez le son cible -', ['center'], ['center'], [200 200 200]);
+    DrawFormattedText(wPtr, '\n READY TO START \n \n - Dï¿½tectez le son cible -', ['center'], ['center'], [200 200 200]);
     Screen('Flip', wPtr);
 
     waitForTrigger(cfg); % this is the function from CPP github
@@ -154,7 +112,7 @@ try  % safety loop: close the screen if code crashes
     Screen('Flip', wPtr);
 
     LoopStart = GetSecs();
-    trial_start = LoopStart; % début du run
+    trial_start = LoopStart; % dï¿½but du run
 
     WaitSecs(8); % wait 8sec at the beginning of the run
 
@@ -250,7 +208,7 @@ try  % safety loop: close the screen if code crashes
 
             %             %%%%%to make a quick check%%%%REMOVE WHILE TESTING
             %         disp(strcat('delay to onset stim: ', num2str(time_stim-Stim_start)));
-            %         disp(strcat('trial duration à partir du lancement du son: ', num2str(loop_end-time_stim)));
+            %         disp(strcat('trial duration ï¿½ partir du lancement du son: ', num2str(loop_end-time_stim)));
             %         disp(strcat('trial duration en comptant toute la boucle stim(load le son etc): ', num2str(loop_end-Stim_start)));
             %         disp(strcat('delay between (time_stim+trial_duration) et loop_end: ', num2str(loop_end-(time_stim+trial_duration))));
 
@@ -337,7 +295,7 @@ try  % safety loop: close the screen if code crashes
     disp(strcat('Timing : the run took (min)', num2str((loop_duration) / 60)));
     disp(strcat('Timing : the run took (sec)', num2str(loop_duration)));
 
-    % FILL the time difference with baseline (due to random n° of targets)
+    % FILL the time difference with baseline (due to random nï¿½ of targets)
     % Draw THE FIX CROSS
     Screen('DrawLines', wPtr, crossLines, crossWidth, crossColor, [screenCenterX, screenCenterY]);
     % Flip the screen
